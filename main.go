4213c3ccdd32759ec39229656636705ac463f113
6nhuseyin <6nhuseyin.gmail.com>
2023-09-20 18:16:05 +0100

    by the O

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	//database driver for SQLite ->
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	database, err := sql.Open("sqlite3", "forumdatabase.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, email TEXT, password TEXT)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	statement.Exec()
+
+	statement, err = database.Prepare("INSERT INTO users(username, email, password) VALUES(?, ? , ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	statement.Exec("bambi", "bambi@gmail.com", "IloveBonesandSometimesMike")
+
+	rows, _ := database.Query("SELECT id, username, email, password FROM users")
+	var id int
+	var username string
+	var password string
+	var email string
+
+	for rows.Next() {
+		rows.Scan(&id, &username, &password, &email)
+
+		fmt.Println("id is: ", id)
+		fmt.Println("username is: ", username)
+		fmt.Println("email is: ", email)
+		fmt.Println("password is: ", password)
+
+	}
+}
