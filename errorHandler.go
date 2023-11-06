e772aa65fdc60494ff90dc71bef4c55128454d50
Diana Adamczyk <112947016+diddydeeable@users.noreply.github.com>
2023-11-06 13:20:00 +0000

    Create errorHandler.go
    Generalised Error message

---
+++ errorHandler.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func ErrorHandler(w http.ResponseWriter, r *http.Request, status int){
+w.WriteHeader(status)
+if status == http.StatusNotFound {
+	fmt.Fprint(w,"404 page not found")
+}
+}
